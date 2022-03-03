9ed2e7e5fe2b363cc123606ed53c827899e3dd0e
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2022-03-03 08:38:36 -0300

    people database created. inserting data on db

--- database.py
+++ database.py
@@ -1,12 +1,33 @@
 import sqlite3
 
-conn = sqlite3.connect("employee.db")
-
+conn = sqlite3.connect("people.db")
 c = conn.cursor()
+
+def create_table():
+    c.execute("""CREATE TABLE person(
+        first_name text,
+        last_name text,
+        email text,
+        age integer,
+        salary real
+    )
+    """)
+
+    conn.commit()
+
+
+def insert_data():
+    manyperson = [
+        ('Wesley', 'Saladao', 'wesleysaladao@brasil.com', 24, 3500.5),
+    ]
+
+    c.executemany("INSERT INTO person VALUES(?,?,?,?,?)", manyperson)
+
+    conn.commit()
 
 
 def show_data():
-    c.execute("SELECT * FROM employee")
+    c.execute("SELECT * FROM person")
     items = c.fetchall()
 
     for item in items:
@@ -14,4 +35,8 @@ def show_data():
 
     conn.commit()
     conn.close()
+
+
+
+
 
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ def menu():
     print("----------------------------\n PLEASE BEGIN YOUR REGISTER\n----------------------------\n")
 
 menu()
-
+database.create_table()
+database.insert_data()
 database.show_data()
